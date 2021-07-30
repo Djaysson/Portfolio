396f979c29f45ae369566ffa3d793b357aebba26
Djaysson <djayson.dev@gmail.com>
2021-07-30 17:16:13 -0300

    Creation of project footer

---
+++ src/components/footer/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Container } from './styles';
+import Background from '../../assets/images/backgroundColo.svg';
+
+function Footer() {
+    return (
+        <Container>
+            <img src={Background} alt="" />
+        </Container>
+    );
+}
+
+export default Footer;
---
+++ src/components/footer/styles.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+export const Container = styled.footer`
+  position: fixed;
+  width: 100%;
+  bottom:0px;
+  height: 6vh;
+ 
+
+img{
+  object-fit:cover;
+  z-index: 0;
+  width: 100%;
+  height: 100%;
+ 
+}
+`;
+
