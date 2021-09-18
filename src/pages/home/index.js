9b793e8fcafb3d996c818e2c1393148127663ce7
Djaysson <djayson.dev@gmail.com>
2021-09-17 22:16:14 -0300

    Making the home page responsive

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,43 +1,44 @@
-import React from 'react';
-import HeaderLogo from '../../components/headerLogo/index';
+import React from "react";
+import HeaderLogo from "../../components/headerLogo/index";
 import Typical from "react-typical";
-import { Container, Section, Button } from './styles';
-import Image from '../../assets/images/image.png';
-import AnimatedBackground from '../../components/background/index'
+import { Container, Section, Button } from "./styles";
+import Image from "../../assets/images/image.png";
+import AnimatedBackground from "../../components/background/index";
 import { FaAngleDoubleDown } from "react-icons/fa";
 
 function Home() {
-    return (
-        <>
-        <AnimatedBackground />
-        <Container>
-            <HeaderLogo />
-            <Section>
-                <h1>Seja bem-vindo</h1>
-                <strong>
-                    Eu sou
-                    <Typical
-                        loop={Infinity}
-                        wrapper="b"
-                        steps={[
-                            " Djayson Rodrigues",
-                            2000,
-                            " um Frontend Developer.",
-                            2000,
-                            " um Freelancer.",
-                            2000,
-                        ]}
-                    />
-                </strong>
-                <Button to="/sobre">
-                    <strong>Saiba mais... <FaAngleDoubleDown size={25}/> </strong>     
-                </Button>
-
-            </Section>
-            <img src={Image} alt="background" />
-        </Container>
-        </>
-    );
+  return (
+    <>
+      <AnimatedBackground />
+      <Container>
+        <HeaderLogo />
+        <Section>
+          <h1>Seja bem-vindo</h1>
+          <strong>
+            Eu sou
+            <Typical
+              loop={Infinity}
+              wrapper="b"
+              steps={[
+                "Djayson Rodrigues",
+                3000,
+                "um Frontend Developer",
+                3000,
+                "um Freelancer",
+                3000,
+              ]}
+            />
+          </strong>
+          <Button to="/sobre">
+            <strong>
+              Saiba mais... <FaAngleDoubleDown size={25} />{" "}
+            </strong>
+          </Button>
+        </Section>
+        <img src={Image} alt="background" />
+      </Container>
+    </>
+  );
 }
 
 export default Home;
--- src/pages/home/styles.js
+++ src/pages/home/styles.js
@@ -1,78 +1,195 @@
-import { Link } from 'react-router-dom';
-import styled from 'styled-components';
+import { Link } from "react-router-dom";
+import styled from "styled-components";
 export const Container = styled.div`
   width: 100vw;
-  height:100vh;
+  height: 100vh;
   display: flex;
   flex-direction: column;
-  justify-content: space-between;
-  animation: bounceInDown; 
-  animation-duration: 2s; 
-img:nth-child(3){
+  justify-content: center;
+  animation: bounceInDown;
+  animation-duration: 2s;
+  img:nth-child(3) {
     position: relative;
-    filter: drop-shadow(2px 2px 6px ${props => props.theme.darkTransparent});
+    filter: drop-shadow(2px 2px 6px ${(props) => props.theme.darkTransparent});
     margin: auto;
     width: 55vw;
     height: 55vh;
     top: -2px;
-    animation: zoomIn; 
-  animation-duration: 4s; 
+    animation: zoomIn;
+    animation-duration: 4s;
+  }
+  @media (min-width: 1025px) and (max-width: 1440px) {
+    overflow-x: hidden;
+
+    img:nth-child(3) {
+      filter: drop-shadow(2px 2px 4px ${(props) => props.theme.secondary});
+      margin: auto;
+      width: 55vw;
+      height: 50vh;
+      top: 10px;
+      padding: 10px 0px;
+    }
+  }
+  @media (min-width: 768px) and (max-width: 1024px) {
+    overflow-x: hidden;
+    header:nth-child(1) {
+      display: none;
+    }
+    img:nth-child(3) {
+      filter: drop-shadow(2px 2px 4px ${(props) => props.theme.secondary});
+      margin: auto;
+      width: 90vw;
+      height: 50vh;
+      top: 20px;
+      padding: 5px 0px;
+    }
+  }
+  @media (min-width: 300px) and (max-width: 767px) {
+    overflow-x: hidden;
+
+    header:nth-child(1) {
+      display: none;
+    }
+    img:nth-child(3) {
+      filter: drop-shadow(2px 2px 4px ${(props) => props.theme.secondary});
+      margin: auto;
+      width: 90vw;
+      height: 45vh;
+      top: 10px;
+      padding: 10px 0px;
+    }
   }
 `;
 export const Section = styled.section`
-margin-top: 80px;
+  margin-top: 80px;
   display: flex;
   justify-content: center;
   align-items: center;
   flex-direction: column;
-  h1{
-  font-size: 30px;
-  font-weight: 500;
+  h1 {
+    font-size: 30px;
+    font-weight: 500;
   }
-  strong{
-  font-size: 50px;
-  font-weight: 700;
-    b{
-      color: ${props => props.theme.secondary};
+  strong {
+    font-size: 60px;
+    font-weight: 700;
+    b {
+      padding: 10px;
+      color: ${(props) => props.theme.secondary};
     }
   }
-  
+  @media (min-width: 1025px) and (max-width: 1440px) {
+    overflow-y: hidden;
+    overflow-x: hidden;
+    margin-top: 80px;
+    h1 {
+      font-size: 20px;
+      font-weight: 500;
+    }
+    strong {
+      font-size: 42px;
+      font-weight: 700;
+      b {
+        color: ${(props) => props.theme.secondary};
+      }
+    }
+  }
+  @media (min-width: 768px) and (max-width: 1024px) {
+    margin-top: 100px;
+    h1 {
+      font-size: 20px;
+      font-weight: 500;
+    }
+    strong {
+      font-size: 45px;
+      font-weight: 700;
+      b {
+        color: ${(props) => props.theme.secondary};
+      }
+    }
+  }
+  @media (min-width: 300px) and (max-width: 767px) {
+    margin-top: 100px;
+    h1 {
+      font-size: 16px;
+      font-weight: 500;
+    }
+    strong {
+      font-size: 21px;
+      font-weight: 700;
+      b {
+        color: ${(props) => props.theme.secondary};
+      }
+    }
+  }
 `;
 export const Button = styled(Link)`
-  margin-top: 10px;
+  margin: 20px 0px;
   transition: transform 2s;
-  z-index: 1;
-  strong{
+
+  strong {
     border: none;
-    background-color: ${props => props.theme.secondary};
-    box-shadow: 0px 0px 15px ${props => props.theme.darkTransparent};
+    background-color: ${(props) => props.theme.secondary};
+    box-shadow: 0px 0px 15px ${(props) => props.theme.darkTransparent};
     border-radius: 15px;
-      color: #FFFFFF;
-      font-size: 18px;
-      font-weight: 500;
-      padding: 10px;
-      svg{
-        margin: auto;
-        animation: movingAnimation 2s  infinite;
-      }
-    &:hover{
-      background: ${props => props.theme.primary};
+    color: #ffffff;
+    font-size: 18px;
+    font-weight: 500;
+    padding: 10px;
+    svg {
+      margin: auto;
+      animation: movingAnimation 2s linear infinite;
+    }
+    &:hover {
+      background: ${(props) => props.theme.primary};
     }
   }
-  &:hover{
-      -ms-transform: scale(1.1); 
-        -webkit-transform: scale(1.1); 
-        transform: scale(1.1);
-    }
-    @keyframes movingAnimation {
+  &:hover {
+    -ms-transform: scale(1.1);
+    -webkit-transform: scale(1.1);
+    transform: scale(1.1);
+  }
+  @keyframes movingAnimation {
     0% {
       opacity: 1;
-      transform:  translateY(5px);
-      
+      transform: translateY(0px);
     }
     100% {
       opacity: 0;
-      transform: translateY(20px);
+      transform: translateY(15px);
+    }
+  }
+  @media (min-width: 1025px) and (max-width: 1440px) {
+    margin: 20px 0px;
+    z-index: 1;
+    strong {
+      font-size: 18px;
+      padding: 15px;
+      svg:nth-child(1) {
+        height: 23px;
+      }
+    }
+  }
+  @media (min-width: 768px) and (max-width: 1024px) {
+    margin-top: 20px;
+    strong {
+      font-size: 16px;
+      padding: 15px;
+      svg:nth-child(1) {
+        height: 22px;
+      }
+    }
+  }
+
+  @media (min-width: 300px) and (max-width: 767px) {
+    margin-top: 15px;
+    cursor: pointer;
+    strong {
+      font-size: 14px;
+      padding: 8px;
+      svg:nth-child(1) {
+        height: 20px;
+      }
     }
   }
 `;
