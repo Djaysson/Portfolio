4ce3bf66794d28dec5452715eeafe965c6538a59
Djaysson <djayson.dev@gmail.com>
2021-09-20 17:42:30 -0300

    Add responsive design to the about page

--- src/pages/sobre/index.js
+++ src/pages/sobre/index.js
@@ -1,50 +1,76 @@
-import React from 'react';
-import HeaderLogo from '../../components/headerLogo/index';
-import { Container, Section, Aside, Button } from './styles';
-import TitlePage from '../../components/titlePage/index';
-import img from '../../assets/images/eu1.png'
-import AnimatedBackground from '../../components/background/index'
+import React from "react";
+import HeaderLogo from "../../components/headerLogo/index";
+import { Container, Section, Aside, Button } from "./styles";
+import TitlePage from "../../components/titlePage/index";
+import img from "../../assets/images/eu1.png";
+import AnimatedBackground from "../../components/background/index";
 
 function Sobre() {
-    return (
-        <>
-        <AnimatedBackground />
-        <Container>
-            <HeaderLogo />
-            <TitlePage subtitle="Sobre Mim" title="Conheça-me mais" />
-            <Section>
-                <div>
-                    <h1>Eu sou <strong>Djayson Rodrigues</strong>, um Frontend Developer</h1>
-                    <p> Minha formação é em Análise e Desenvolvimento de sistemas na Universidade Estácio, sou uma pessoa apaixonado pelo desenvolvimento de software e fascinado por novos desafios na área TI, estou envolvido na área a 3 anos e sou do tipo de pessoa que adora uma boa UX e UI agradáveis ao usuário.  </p>
-                    <p> Um desenvolvedor que visa promover a entregar produtos da melhor qualidade. Alguém que acredita que perseverança, esforço e determinação levam aos melhores resultados. Estou sempre focado em provar as melhores práticas, fundamentos e consistência através do código.</p>
-                    <p> Venha ver alguns dos meus projetos e se você tem algum projeto a ser feito, eu posso ajuda a construir o seu projeto, tudo isso a um preço acessível. </p>
-                    
-                </div>
-                <Aside>
-                    <ul>
-                        <li>Nome:
-                            <span>Djayson Rodrigues</span>
-                        </li>
-                        <li>
-                            Cidade:
-                            <span>Fortaleza, Ceara</span>
-                        </li>
-                        <li>
-                            Email:
-                            <span><a href="mailto:djayson.dev@gmail.com"> djayson.dev@gmail.com</a></span>
-                        </li>
-                    </ul>
+  return (
+    <>
+      <AnimatedBackground />
+      <Container>
+        <HeaderLogo />
+        <TitlePage subtitle="Sobre Mim" title="Conheça-me mais" />
+        <Section>
+          <div>
+            <h1>
+              Eu sou <strong>Djayson Rodrigues</strong>, um Frontend Developer
+            </h1>
+            <p>
+              Minha formação é em Análise e Desenvolvimento de sistemas na
+              Universidade Estácio, sou uma pessoa apaixonado pelo
+              desenvolvimento de software e fascinado por novos desafios na área
+              TI, estou envolvido na área a 3 anos e sou do tipo de pessoa que
+              adora uma boa UX e UI agradáveis ao usuário.{" "}
+            </p>
+            <p>
+              Um desenvolvedor que visa promover a entregar produtos da melhor
+              qualidade. Alguém que acredita que perseverança, esforço e
+              determinação levam aos melhores resultados. Estou sempre focado em
+              provar as melhores práticas, fundamentos e consistência através do
+              código.
+            </p>
+            <p>
+              Venha ver alguns dos meus projetos e se você tem algum projeto a
+              ser feito, eu posso ajuda a construir o seu projeto, tudo isso a
+              um preço acessível.
+            </p>
+          </div>
+          <Aside>
+            <ul>
+              <li>
+                Nome:
+                <span>Djayson Alves Rodrigues</span>
+              </li>
+              <li>
+                Cidade:
+                <span>Fortaleza, Ceará</span>
+              </li>
+              <li>
+                Email:
+                <span>
+                  <a href="mailto:djayson.dev@gmail.com">
+                    {" "}
+                    djayson.dev@gmail.com
+                  </a>
+                </span>
+              </li>
+            </ul>
 
-                    <Button href="https://drive.google.com/file/d/1BlQascd2UED4FeVPEJk88prpn4N84QpR/view?usp=sharing" target="blank" rel="noopener noreferrer">
-                        <strong>📋 Currículo</strong>
-                    </Button>
-                </Aside>
-            </Section>
-            <img src={img} alt="Imagem de fundo" />
-
-        </Container>
+            <Button
+              href="https://drive.google.com/file/d/1BlQascd2UED4FeVPEJk88prpn4N84QpR/view?usp=sharing"
+              target="blank"
+              rel="noopener noreferrer"
+            >
+              <strong>📋 Currículo</strong>
+            </Button>
+          </Aside>
+        </Section>
+        <img src={img} alt="Imagem de fundo" />
+      </Container>
     </>
-    );
+  );
 }
 
 export default Sobre;
--- src/pages/sobre/styles.js
+++ src/pages/sobre/styles.js
@@ -1,142 +1,250 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const Container = styled.div`
-width: 100vw;
-height: 100vh;
-display: flex;
-flex-direction:column;
-animation: bounceInDown; 
-animation-duration: 2s; 
-overflow: hidden;
-
-img:nth-child(4){
-  position: relative;
-  width: 40vw;
+  width: 100vw;
   height: 100vh;
-  right: -680px;
-  bottom: 380px;
-  z-index: 0;
-  animation: animatedImage 4s alternate infinite;
+  display: flex;
+  flex-direction: column;
+  animation: bounceInDown;
+  animation-duration: 2s;
+  overflow: hidden;
 
-  @keyframes animatedImage{
-    0% {
-      filter: drop-shadow(0px);
-      transform:  translateX(0px);
-    
-    }
-    100% {
-      filter: drop-shadow(100px -60px 100px ${props => props.theme.primary});
-      transform: translateX(-90px);
-      
+  img:nth-child(4) {
+    position: absolute;
+    width: 40vw;
+    height: 75vh;
+    right: 0px;
+    bottom: 0px;
+    animation: animatedImage 4s alternate infinite;
+
+    @keyframes animatedImage {
+      from {
+        filter: drop-shadow(
+          -40px 40px 40px ${(props) => props.theme.secondary}
+        );
+      }
+      to {
+        filter: drop-shadow(40px 40px 40px ${(props) => props.theme.primary});
+      }
     }
   }
-}
+  @media (min-width: 768px) and (max-width: 1024px) {
+    overflow-y: initial;
+    overflow-x: hidden;
+    animation: bounceInDown;
+    animation-duration: 2s;
+    img:nth-child(4) {
+      height: 62vh;
+      z-index: -1;
+    }
+  }
+
+  @media (min-width: 300px) and (max-width: 767px) {
+    overflow-y: initial;
+    overflow-x: hidden;
+    img:nth-child(4) {
+      display: none;
+    }
+  }
 `;
 export const Section = styled.section`
   display: flex;
   flex-direction: row;
   margin-top: 198px;
   z-index: 1;
-  
-div{
-  flex-direction: column;
-  width: 50vw;
-  margin-left: 20px;
-  h1{
-    margin-bottom: 20px;
-    font-size: 25px;
-    font-weight: 700;
-    animation:  bounceInDown; 
-    animation-duration: 8s; 
+
+  div {
+    flex-direction: column;
+    width: 50vw;
+    margin-left: 20px;
+    h1 {
+      margin-bottom: 20px;
+      font-size: 25px;
+      font-weight: 700;
+      animation: bounceInDown;
+      animation-duration: 8s;
+    }
+    p {
+      font-size: 16px;
+      text-indent: 10px;
+      line-height: 20px;
+      text-align: left;
+      font-weight: 500;
+      margin-bottom: 15px;
+    }
+    p:nth-child(2) {
+      animation: bounceInDown;
+      animation-duration: 6s;
+    }
+    p:nth-child(3) {
+      animation: bounceInDown;
+      animation-duration: 4s;
+    }
+    p:nth-child(4) {
+      animation: bounceInDown;
+      animation-duration: 2s;
+    }
+    strong {
+      color: ${(props) => props.theme.secondary};
+    }
   }
-  p{
-    font-size:16px; 
-    text-indent:10px;
-    line-height: 20px;
-    text-align: left;
-    font-weight: 500;
-    margin-bottom: 15px;
+  @media (min-width: 768px) and (max-width: 1024px) {
+    justify-content: center;
+    margin-top: 150px;
+    z-index: 0;
   }
-  p:nth-child(2){
-    animation:bounceInDown;  
-    animation-duration: 6s; 
+
+  @media (min-width: 300px) and (max-width: 767px) {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    margin-top: 100px;
+    z-index: 0;
+
+    div {
+      width: 90vw;
+      margin: 0 auto;
+      h1 {
+        margin-bottom: 10px;
+        font-size: 18px;
+        font-weight: 700;
+        animation: fadeInLeft;
+        animation-duration: 2s;
+      }
+      p {
+        font-size: 15px;
+        text-indent: 10px;
+        line-height: 20px;
+        text-align: left;
+        margin-bottom: 10px;
+      }
+      p:nth-child(2) {
+        animation: fadeInLeft;
+        animation-duration: 1s;
+      }
+      p:nth-child(3) {
+        animation: fadeInLeft;
+        animation-duration: 2s;
+      }
+      p:nth-child(4) {
+        animation: fadeInLeft;
+        animation-duration: 3s;
+      }
+      strong {
+        color: ${(props) => props.theme.secondary};
+      }
+    }
   }
-  p:nth-child(3){
-    animation:bounceInDown; 
-    animation-duration: 4s; 
-  }
-  p:nth-child(4){
-  animation:bounceInDown; 
-  animation-duration: 2s; 
-  }
-  strong{
-    color: ${props => props.theme.secondary};
-  }
-}
 `;
 export const Aside = styled.aside`
   margin-left: 40px;
   margin-top: 50px;
   width: 25vw;
-  ul{
+  ul {
     list-style: none;
-    li{
+    li {
       font-size: 16px;
       font-weight: 500;
       margin-bottom: 30px;
-      border-bottom: 1px solid ${props => props.theme.secondary};
+      border-bottom: 1px solid ${(props) => props.theme.secondary};
     }
-    li:nth-child(1){
-      animation:bounceInDown; 
-      animation-duration: 8s; 
+    li:nth-child(1) {
+      animation: bounceInDown;
+      animation-duration: 8s;
     }
-    li:nth-child(2){
-      animation:bounceInDown; 
-      animation-duration: 6s; 
+    li:nth-child(2) {
+      animation: bounceInDown;
+      animation-duration: 6s;
     }
-    li:nth-child(3){
-      animation:bounceInDown; 
-      animation-duration: 4s; 
+    li:nth-child(3) {
+      animation: bounceInDown;
+      animation-duration: 4s;
     }
-    span{
+    span {
       padding-left: 4px;
-        
-      a{
+
+      a {
         transition: color.2s;
-        color: ${props => props.theme.font};
-      &:hover{
-          color: ${props => props.theme.secondary};
+        color: ${(props) => props.theme.font};
+        &:hover {
+          color: ${(props) => props.theme.secondary};
         }
       }
-  } }
+    }
+  }
+  @media (min-width: 768px) and (max-width: 1024px) {
+    z-index: 1;
+  }
+  @media (min-width: 300px) and (max-width: 767px) {
+    margin: 0 auto;
+    width: 90vw;
+    ul {
+      margin-top: 20px;
+      list-style: none;
+      li {
+        font-size: 16px;
+        margin-bottom: 20px;
+      }
+      li:nth-child(1) {
+        animation: fadeInLeft;
+        animation-duration: 1s;
+      }
+      li:nth-child(2) {
+        animation: fadeInLeft;
+        animation-duration: 2s;
+      }
+      li:nth-child(3) {
+        animation: fadeInLeft;
+        animation-duration: 3s;
+      }
+      span {
+        padding-left: 4px;
+        font-size: 14px;
+      }
+    }
+  }
 `;
-
 export const Button = styled.a`
-display:  flex;
-align-items: center;
-justify-content: center;
-transition: transform 1s;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  transition: transform 1s;
 
-  strong{
+  strong {
     width: 25vw;
     text-align: center;
-    background: ${props => props.theme.secondary};
-    box-shadow: 0px 0px 15px ${props => props.theme.darkTransparent};
-    border-radius: 15px;
-    color: ${props => props.theme.font};
+    background: ${(props) => props.theme.secondary};
+    box-shadow: 0px 0px 15px ${(props) => props.theme.darkTransparent};
+    border-radius: 10px;
+    color: ${(props) => props.theme.font};
     font-size: 18px;
     font-weight: 500;
     padding: 5px;
     margin: 5px;
-    animation:bounceInDown; 
-    animation-duration: 2s; 
-    &:hover{
-      background: ${props => props.theme.primary};
+    animation: bounceInDown;
+    animation-duration: 2s;
+    &:hover {
+      background: ${(props) => props.theme.primary};
     }
   }
-  &:hover{
-    -ms-transform: scale(1.1); 
-    -webkit-transform: scale(1.1); 
+  &:hover {
+    -ms-transform: scale(1.1);
+    -webkit-transform: scale(1.1);
     transform: scale(1.1);
+  }
+  @media (min-width: 300px) and (max-width: 767px) {
+    strong {
+      width: 34vw;
+      border-radius: 10px;
+      font-size: 16px;
+      padding: 7px;
+      margin: 7px;
+      animation: fadeInLeft;
+      animation-duration: 3s;
+      &:hover {
+        background: ${(props) => props.theme.primary};
+      }
+    }
   }
 `;
