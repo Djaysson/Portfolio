2e3dd4f94eecac96cf34a85c5078cff1ae5dbbb0
Djaysson <djayson.dev@gmail.com>
2021-09-21 19:01:35 -0300

    Add a responsive design to the summary page

--- src/pages/resumo/index.js
+++ src/pages/resumo/index.js
@@ -1,157 +1,156 @@
-import React from 'react';
-import { Container, Section, Skills } from './styles';
-import HeaderLogo from '../../components/headerLogo/index'
+import React from "react";
+import { Container, Section, Skills } from "./styles";
+import HeaderLogo from "../../components/headerLogo/index";
 import LogoEstacio from "../../assets/logos/estacio.png";
 import LogoFreeCode from "../../assets/logos/free.png";
 import LogoDigital from "../../assets/logos/digital.png";
 import { SiRedux } from "react-icons/si";
-import TitlePage from '../../components/titlePage/index';
-import AnimatedBackground from '../../components/background/index'
+import TitlePage from "../../components/titlePage/index";
+import AnimatedBackground from "../../components/background/index";
 
 import {
-    FaHtml5,
-    FaCss3Alt,
-    FaJsSquare,
-    FaBootstrap,
-    FaGitSquare,
-    FaReact,
-    FaUserAlt,
-    FaHandshake,
-    FaHeart,
-    FaTools,
-    FaMicrophone,
-    FaLightbulb,
-    FaNode
+  FaHtml5,
+  FaCss3Alt,
+  FaJsSquare,
+  FaBootstrap,
+  FaGitSquare,
+  FaReact,
+  FaUserAlt,
+  FaHandshake,
+  FaHeart,
+  FaTools,
+  FaMicrophone,
+  FaLightbulb,
+  FaNode,
 } from "react-icons/fa";
 
-
 function Resumo() {
-    return (
-        <>
-        <AnimatedBackground />
-        <Container>
-            <HeaderLogo />
-            <TitlePage subtitle="Resumo" title="Resumir" />
-            <Section>
-                <h1>Minha Educação</h1>
-                <div>
-                    <div>
-                        <span>2018 - 2020</span>
-                        <p>Análise e Desenvolvimento de Sistemas</p>
-                        <p>Universidade Estácio</p>
-                        <a
-                            href="https://estacio.br/cursos/graduacao/analise-e-desenvolvimento-de-sistemas"
-                            target="blank"><img src={LogoEstacio} alt="Logo Estácio" /> </a>
-                    </div>
-                    <div>
-                        <span>2020 - 2021</span>
-                        <p>JavaScript Algorithms and Data Structures</p>
-                        <p>FreeCodeCamp</p>
-                        <a
-                            href="https://www.freecodecamp.org/certification/fcc07f30d36-40e3-46b4-a612-b085a50d2773/javascript-algorithms-and-data-structures"
-                            target="blank"><img src={LogoFreeCode} alt="Logo Estácio" /> </a>
-                    </div>
-                    <div>
-                        <span>2020 - 2021</span>
-                        <p>bootcamp Become Remote</p>
-                        <p>Digital Innovation One</p>
-                        <a
-                            href="https://certificates.digitalinnovation.one/6CF589DE"
-                            target="blank"><img src={LogoDigital} alt="Logo Estácio" /> </a>
-                    </div>
-                    <div>
-                        <span>2020 - 2021</span>
-                        <p>Responsive Web Design</p>
-                        <p>FreeCodeCamp</p>
-                        <a
-                            href="https://www.freecodecamp.org/certification/fcc07f30d36-40e3-46b4-a612-b085a50d2773/responsive-web-design"
-                            target="blank"><img src={LogoFreeCode} alt="Logo Estácio" /> </a>
-                    </div>
-
-
-                </div>
-
-            </Section>
-            <Skills>
-                <div>
-                    <h1>Soft Skills</h1>
-                    <ul>
-                        <li>
-                            <FaUserAlt size={20} />
-                            <span>Foco no cliente</span>
-                        </li>
-                        <li>
-                            <FaHandshake size={20} />
-                            <span>Trabalho em Equipe</span>
-                        </li>
-                        <li>
-                            <FaMicrophone size={20} />
-                            <span>Comunicação</span>
-                        </li>
-                        <li>
-                            <FaLightbulb size={20} />
-                            <span>Problem-solving</span>
-                        </li>
-                        <li>
-                            <FaHeart size={20} />
-                            <span>Paixão por aprender</span>
-                        </li>
-                        <li>
-                            <FaTools size={20} />
-                            <span>Persistência</span>
-                        </li>
-                    </ul>
-                </div>
-                <div>
-                    <h1>Hard Skills</h1>
-                    <ul>
-                        <li>
-                            <FaHtml5 size={20} />
-                            <span>HTML</span>
-
-                        </li>
-                        <li>
-                            <FaCss3Alt size={20} />
-                            <span>CSS</span>
-
-                        </li>
-                        <li>
-                            <FaJsSquare size={20} />
-                            <span>JAVASCRIPT</span>
-
-                        </li>
-                        <li>
-                            <FaBootstrap size={20} />
-                            <span>BOOTSTRAP</span>
-
-                        </li>
-                        <li>
-                            <FaReact size={20} />
-                            <span>REACT</span>
-
-                        </li>
-                        <li>
-                            <SiRedux size={20} />
-                            <span>REDUX</span>
-
-                        </li>
-                        <li>
-                            <FaGitSquare size={20} />
-                            <span>GIT</span>
-
-                        </li>
-                        <li>
-                            <FaNode size={20} />
-                            <span>NODE</span>
-
-                        </li>
-                    </ul>
-                </div>
-            </Skills>
-
-        </Container>
-        </>
-    );
+  return (
+    <>
+      <AnimatedBackground />
+      <Container>
+        <HeaderLogo />
+        <TitlePage subtitle="Resumo" title="Resumir" />
+        <Section>
+          <h1>Minha Educação</h1>
+          <div>
+            <div>
+              <span>2018 - 2020</span>
+              <p>Análise e Desenvolvimento de Sistemas</p>
+              <p>Universidade Estácio</p>
+              <a
+                href="https://estacio.br/cursos/graduacao/analise-e-desenvolvimento-de-sistemas"
+                target="blank"
+              >
+                <img src={LogoEstacio} alt="Logo Estácio" />{" "}
+              </a>
+            </div>
+            <div>
+              <span>2020 - 2021</span>
+              <p>JavaScript Algorithms and Data Structures</p>
+              <p>FreeCodeCamp</p>
+              <a
+                href="https://www.freecodecamp.org/certification/fcc07f30d36-40e3-46b4-a612-b085a50d2773/javascript-algorithms-and-data-structures"
+                target="blank"
+              >
+                <img src={LogoFreeCode} alt="Logo Estácio" />{" "}
+              </a>
+            </div>
+            <div>
+              <span>2020 - 2021</span>
+              <p>Bootcamp Become Remote</p>
+              <p>Digital Innovation One</p>
+              <a
+                href="https://certificates.digitalinnovation.one/6CF589DE"
+                target="blank"
+              >
+                <img src={LogoDigital} alt="Logo Estácio" />{" "}
+              </a>
+            </div>
+            <div>
+              <span>2020 - 2021</span>
+              <p>Responsive Web Design</p>
+              <p>FreeCodeCamp</p>
+              <a
+                href="https://www.freecodecamp.org/certification/fcc07f30d36-40e3-46b4-a612-b085a50d2773/responsive-web-design"
+                target="blank"
+              >
+                <img src={LogoFreeCode} alt="Logo Estácio" />{" "}
+              </a>
+            </div>
+          </div>
+        </Section>
+        <Skills>
+          <div>
+            <h1>Soft Skills</h1>
+            <ul>
+              <li>
+                <FaUserAlt size={20} />
+                <span>Foco no cliente</span>
+              </li>
+              <li>
+                <FaHandshake size={20} />
+                <span>Trabalho em Equipe</span>
+              </li>
+              <li>
+                <FaMicrophone size={20} />
+                <span>Comunicação</span>
+              </li>
+              <li>
+                <FaLightbulb size={20} />
+                <span>Problem-solving</span>
+              </li>
+              <li>
+                <FaHeart size={20} />
+                <span>Paixão por aprender</span>
+              </li>
+              <li>
+                <FaTools size={20} />
+                <span>Persistência</span>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <h1>Hard Skills</h1>
+            <ul>
+              <li>
+                <FaHtml5 size={20} />
+                <span>Html</span>
+              </li>
+              <li>
+                <FaCss3Alt size={20} />
+                <span>Css</span>
+              </li>
+              <li>
+                <FaJsSquare size={20} />
+                <span>Javascript</span>
+              </li>
+              <li>
+                <FaBootstrap size={20} />
+                <span>Bootstrap</span>
+              </li>
+              <li>
+                <FaReact size={20} />
+                <span>React</span>
+              </li>
+              <li>
+                <SiRedux size={20} />
+                <span>Redux</span>
+              </li>
+              <li>
+                <FaGitSquare size={20} />
+                <span>Git</span>
+              </li>
+              <li>
+                <FaNode size={20} />
+                <span>Node</span>
+              </li>
+            </ul>
+          </div>
+        </Skills>
+      </Container>
+    </>
+  );
 }
 
 export default Resumo;
--- src/pages/resumo/styles.js
+++ src/pages/resumo/styles.js
@@ -1,144 +1,430 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const Container = styled.div`
   width: 100vw;
   height: 100vh;
-  animation: bounceInDown; 
-  animation-duration: 2s;  
-`;
+  animation: bounceInDown;
+  animation-duration: 2s;
+  overflow: hidden;
+  @media (min-width: 768px) and (max-width: 1024px) {
+    overflow-y: initial;
+  }
 
+  @media (min-width: 300px) and (max-width: 767px) {
+    overflow-y: initial;
+  }
+`;
 export const Section = styled.section`
-  margin: 0 auto;
-  margin-top: 130px;
+  margin-top: 100px;
   display: flex;
   flex-direction: column;
-  padding-left: 85px;
-  animation:  bounceInDown; 
-  animation-duration: 3s; 
-  h1{
-    font-size: 20px;
+  align-items: center;
+  animation: bounceInDown;
+  animation-duration: 3s;
+
+  h1 {
+    font-size: 25px;
     font-weight: 700;
-    margin-bottom: 5px;
-    text-align: left;
-    }
-    div{
+  }
+  div {
     display: grid;
-    grid-template-columns: repeat(2,0.5fr);
-    margin-bottom: 10px;
+    grid-template-columns: repeat(2, 0.5fr);
+    padding: 10px 0px;
+    grid-gap: 15px;
     z-index: 1;
-    div{
-    display: flex;
-    flex-direction: column;
-    border-bottom: 5px solid ${props => props.theme.secondary};
-    background: ${props => props.theme.itemBackgroundColor};
-    box-shadow: 0px 0px 15px ${props => props.theme.darkTransparent};
-    border-radius: 15px;
-    width:30vw;
-    height: 21vh;
-      span{
-      background-color: ${props => props.theme.secondary};
-      text-align: center;
-      margin: 4px 4px 0px 4px;
+    div {
+      display: flex;
+      flex-direction: column;
+      border-bottom: 5px solid ${(props) => props.theme.secondary};
+      background: ${(props) => props.theme.itemBackgroundColor};
+      box-shadow: 0px 0px 15px ${(props) => props.theme.darkTransparent};
+      border-radius: 10px;
+      width: 400px;
+      height: 168px;
+      span {
+        background-color: ${(props) => props.theme.secondary};
+        text-align: center;
+        padding: 4px 0px;
+        font-weight: 500;
       }
-      p{
-        font-size: 16px;
+      p {
         font-weight: 700;
         padding-left: 15px;
-        margin: 3px;
+        margin: -1px 0px;
       }
-      a{
-        font-size: 14px;
-        font-weight: 500; 
-        color: ${props => props.theme.font};
+      p:nth-child(2) {
+        font-size: 18.5px;
+      }
+      p:nth-child(3) {
+        font-size: 16.5px;
+        opacity: 0.7;
+      }
+      a {
+        position: relative;
+      }
+      img {
         transition: transform 1s;
-      img{ 
-        padding-left: 28px;
-        margin-top: 8px;
-        width: 80px;
+        padding-left: 15px;
+        margin: 5px 0px;
+        width: 70px;
         height: 30px;
+
+        &:hover {
+          color: ${(props) => props.theme.secondary};
+          -ms-transform: scale(1.2);
+          -webkit-transform: scale(1.2);
+          transform: scale(1.2);
+        }
       }
-      &:hover{
-        color: ${props => props.theme.secondary};
-        -ms-transform: scale(1.1); 
-        -webkit-transform: scale(1.1); 
-        transform: scale(1.1);
+    }
+  }
+  @media (min-width: 1025px) and (max-width: 1440px) {
+    div {
+      display: grid;
+      grid-template-columns: repeat(2, 0.5fr);
+      padding: 10px 0px;
+      grid-gap: 10px;
+      z-index: 1;
+      div {
+        width: 385px;
+        height: 168px;
       }
+    }
+  }
+
+  @media (min-width: 768px) and (max-width: 1024px) {
+    animation: bounceInDown;
+    animation-duration: 3s;
+    overflow-y: initial;
+    overflow-x: hidden;
+    h1 {
+      font-size: 20px;
+    }
+    div {
+      display: grid;
+      grid-template-columns: repeat(2, 1fr);
+      padding: 10px 0px;
+      margin: 0 -5px;
+      grid-gap: 2em;
+      z-index: 0;
+
+      div {
+        width: 360px;
+        height: 170px;
+        span {
+          text-align: center;
+          padding: 4px 0px;
+        }
+        p {
+          font-weight: 700;
+          padding-left: 15px;
+        }
+        p:nth-child(2) {
+          font-size: 17.5px;
+        }
+        p:nth-child(3) {
+          font-size: 14.5px;
+        }
+        a {
+          position: relative;
+        }
+        img {
+          transition: transform 1s;
+          padding-left: 15px;
+          margin: -5px 0px;
+          width: 60px;
+          height: 25px;
+        }
       }
-    
+    }
+  }
+  @media (min-width: 300px) and (max-width: 767px) {
+    animation: fadeInLeft;
+    animation-duration: 2s;
+    h1 {
+      font-size: 18px;
+    }
+    div {
+      display: grid;
+      grid-template-columns: repeat(1, 1fr);
+      padding: 10px 0px;
+      grid-gap: 10px;
+      z-index: 0;
+
+      div {
+        width: 280px;
+        height: 170px;
+        span {
+          text-align: center;
+          padding: 4px 0px;
+        }
+        p {
+          font-weight: 700;
+          padding-left: 15px;
+          margin: 2px 0px;
+        }
+        p:nth-child(2) {
+          font-size: 17.5px;
+        }
+        p:nth-child(3) {
+          font-size: 14.5px;
+        }
+        a {
+          position: relative;
+        }
+        img {
+          transition: transform 1s;
+          padding-left: 15px;
+          margin: 5px 0px;
+          width: 65px;
+          height: 25px;
+        }
+      }
+      div:nth-child(1) {
+        animation: fadeInLeft;
+        animation-duration: 2s;
+      }
+      div:nth-child(2) {
+        animation: fadeInLeft;
+        animation-duration: 3s;
+      }
+      div:nth-child(3) {
+        animation: fadeInLeft;
+        animation-duration: 4s;
+      }
+      div:nth-child(4) {
+        animation: fadeInLeft;
+        animation-duration: 5s;
+      }
     }
   }
 `;
 export const Skills = styled.footer`
   display: flex;
   flex-direction: row;
-  justify-content: space-between;
-  margin-left: 10px;
-  margin-right: 10px;
-  animation:  bounceInDown; 
-  animation-duration: 2s; 
-div:first-child{
-    z-index: 1; 
-  h1{
-    text-align: center;
-    font-size: 20px;
-    color: ${props => props.theme.font};
+  justify-content: space-around;
+  padding: 10px 0px;
+  animation: bounceInDown;
+  animation-duration: 2s;
+
+  div:first-child {
+    z-index: 1;
+    h1 {
+      text-align: center;
+      font-size: 25px;
+      padding: 10px 0px;
+      color: ${(props) => props.theme.font};
+    }
+    ul {
+      list-style: none;
+      display: grid;
+      grid-template-columns: repeat(3, 1fr);
+      grid-gap: 1em;
+    }
+    li {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+      border-bottom: 5px solid ${(props) => props.theme.secondary};
+      background: ${(props) => props.theme.itemBackgroundColor};
+      box-shadow: 0px 0px 15px ${(props) => props.theme.darkTransparent};
+      border-radius: 15px;
+      width: 130px;
+      height: 60px;
+      padding: 5px;
+      &:hover {
+        color: ${(props) => props.theme.secondary};
+        -ms-transform: scale(1.1);
+        -webkit-transform: scale(1.1);
+        transform: scale(1.1);
+      }
+    }
+    span {
+      font-size: 12px;
+      font-weight: 500;
+      text-align: center;
+    }
   }
-  ul{
-    list-style: none;
-    display: grid;
-    grid-template-columns: repeat(3,0.7fr);
-    grid-gap: 0.3em;
+  div:last-child {
+    z-index: 1;
+    h1 {
+      text-align: center;
+      padding: 10px 0px;
+      font-size: 25px;
+      color: ${(props) => props.theme.font};
+    }
+    ul {
+      list-style: none;
+      display: grid;
+      grid-template-columns: repeat(4, 0.7fr);
+      grid-gap: 1em;
+    }
+    li {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+      border-bottom: 5px solid ${(props) => props.theme.secondary};
+      background: ${(props) => props.theme.itemBackgroundColor};
+      box-shadow: 0px 0px 15px ${(props) => props.theme.darkTransparent};
+      border-radius: 15px;
+      width: 130px;
+      height: 60px;
+      padding: 5px;
+      &:hover {
+        color: ${(props) => props.theme.secondary};
+        -ms-transform: scale(1.1);
+        -webkit-transform: scale(1.1);
+        transform: scale(1.1);
+      }
+    }
+    span {
+      font-size: 12px;
+      font-weight: 500;
+      text-align: center;
+    }
   }
-  li{
+  @media (min-width: 1025px) and (max-width: 1440px) {
+    div {
+      padding: 0px 10px;
+    }
+    div:first-child {
+      z-index: 1;
+      ul {
+        grid-gap: 0.7em;
+      }
+      li {
+        width: 120px;
+        height: 60px;
+        padding: 5px;
+      }
+    }
+    div:last-child {
+      z-index: 1;
+      ul {
+        grid-gap: 0.7em;
+      }
+      li {
+        width: 120px;
+        height: 60px;
+        padding: 5px;
+      }
+    }
+  }
+
+  @media (min-width: 768px) and (max-width: 1024px) {
     display: flex;
     flex-direction: column;
     align-items: center;
-    justify-content: center;
-    border-bottom: 5px solid ${props => props.theme.secondary};
-    background: ${props => props.theme.itemBackgroundColor};
-    box-shadow: 0px 0px 15px ${props => props.theme.darkTransparent};
-    border-radius: 15px;
-    width: 11vw;
-    height: 7vh;
-    padding: 5px;
-    
+    animation: bounceInDown;
+    animation-duration: 2s;
+    div {
+      padding: 0px 20px;
+    }
+    div:first-child {
+      z-index: 0;
+
+      h1 {
+        font-size: 20px;
+        padding: 20px 0px;
+      }
+      ul {
+        display: grid;
+        grid-template-columns: repeat(2, 1fr);
+        grid-gap: 2em;
+      }
+      li {
+        width: 300px;
+        height: 50px;
+      }
+      span {
+        font-size: 11.5px;
+      }
+    }
+    div:last-child {
+      z-index: 0;
+      h1 {
+        font-size: 20px;
+        padding: 20px 0px;
+      }
+      ul {
+        display: grid;
+        grid-template-columns: repeat(3, 1fr);
+        grid-gap: 1em;
+      }
+      li {
+        width: 200px;
+        height: 50px;
+      }
+
+      span {
+        font-size: 12px;
+      }
+    }
   }
-  span{
-    font-size: 12px;
-    font-weight: 500;
-    text-align: center;
-  }
-}
-div:last-child{
-  z-index: 1; 
-  h1{
-    text-align: center;
-    font-size: 20px;
-    color: ${props => props.theme.font};
-  }
-  ul{
-    list-style: none;
-    display: grid;
-    grid-template-columns: repeat(4,0.7fr);
-    grid-gap: 0.3em;
-  }
-  li{
+
+  @media (min-width: 300px) and (max-width: 767px) {
     display: flex;
     flex-direction: column;
+    justify-content: space-between;
     align-items: center;
-    justify-content: center;
-    border-bottom: 5px solid ${props => props.theme.secondary};
-    background: ${props => props.theme.itemBackgroundColor};
-    box-shadow: 0px 0px 15px ${props => props.theme.darkTransparent};
-    border-radius: 15px;
-    width: 11vw;
-    height: 7vh;
-    padding: 5px; 
+    padding: 10px 0px;
+    overflow-y: initial;
+    overflow-x: hidden;
+    animation: fadeInLeft;
+    animation-duration: 5s;
+    div:first-child {
+      z-index: 0;
+      h1 {
+        text-align: center;
+        font-size: 18.5px;
+        padding: 20px 0px;
+      }
+      ul {
+        list-style: none;
+        display: grid;
+        grid-template-columns: repeat(2, 1fr);
+        grid-gap: 2em 3em;
+      }
+      li {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        width: 120px;
+        height: 50px;
+        padding: 5px;
+      }
+      span {
+        font-size: 11px;
+        font-weight: 500;
+        text-align: center;
+      }
+    }
+    div:last-child {
+      z-index: 0;
+      h1 {
+        padding: 20px 0px;
+        font-size: 18.5px;
+      }
+      ul {
+        display: grid;
+        grid-template-columns: repeat(2, 1fr);
+        grid-gap: 2em 3em;
+      }
+      li {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        width: 120px;
+        height: 50px;
+        padding: 5px;
+      }
+      span {
+        font-size: 11px;
+      }
+    }
   }
-  span{
-    font-size: 12px;
-    font-weight: 500;
-    text-align: center;
-  }
-}
 `;
