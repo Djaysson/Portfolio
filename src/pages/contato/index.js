202456e90aa8a7b727831c7dc9e321e1005b86a6
Djaysson <djayson.dev@gmail.com>
2021-09-22 20:14:18 -0300

    Add a responsive design to the project contact page

--- src/pages/contato/index.js
+++ src/pages/contato/index.js
@@ -1,93 +1,130 @@
-import React from 'react';
-import HeaderLogo from '../../components/headerLogo/index'
-import { Container, Section, ContainerContato, ContainerForm, Button } from './styles';
-import Footer from '../../components/footer/index';
-import TitlePage from '../../components/titlePage/index';
-import AnimatedBackground from '../../components/background/index'
+import React from "react";
+import HeaderLogo from "../../components/headerLogo/index";
+import {
+  Container,
+  Section,
+  ContainerContato,
+  ContainerForm,
+  Button,
+} from "./styles";
+import Footer from "../../components/footer/index";
+import TitlePage from "../../components/titlePage/index";
+import AnimatedBackground from "../../components/background/index";
 
 import {
-    FaWhatsappSquare,
-    FaPhoneSquareAlt,
-    FaEnvelopeSquare,
-    FaInstagramSquare,
-    FaLinkedin,
-    FaGithubSquare,
-    FaYoutubeSquare
-} from 'react-icons/fa';
+  FaWhatsappSquare,
+  FaPhoneSquareAlt,
+  FaEnvelopeSquare,
+  FaInstagramSquare,
+  FaLinkedin,
+  FaGithubSquare,
+  FaYoutubeSquare,
+} from "react-icons/fa";
 
 function Contato() {
-    return (
-        <>
-        <AnimatedBackground />
-        <Container>
-            <HeaderLogo />
-            <TitlePage subtitle="Contato" title="Entrar em contato" />
-            <Section>
-                <ContainerContato>
-                    <div>
-                        <h1>Contato alternativo</h1>
-                        <ul>
-                            <li>
-
-                                <FaPhoneSquareAlt size={30} color="#4545E5" />
-                                <a target="blank" href="tel:(85)99177-4335">(85)99177-4335</a>
-
-                            </li>
-                            <li>
-                                <FaWhatsappSquare size={30} color="#4545E5" />
-                                <a target="blank" href={'https://api.whatsapp.com/send?phone=5585991774335'}>Whatsapp</a>
-
-                            </li>
-                            <li>
-                                <FaEnvelopeSquare size={30} color="#4545E5" />
-                                <a target="blank" href="mailto:djayson.dev@gmail.com">Email</a>
-                            </li>
-                        </ul>
-                    </div>
-                    <div>
-                        <h1>Me siga</h1>
-                        <ul>
-                            <li>
-                                <a target="blank" href="https://instagram.com/djayson_alves">
-                                    <FaInstagramSquare size={30} color="#4545E5" />
-                                </a>
-                            </li>
-                            <li>
-
-                                <a target="blank" href="https://www.linkedin.com/in/djaysonrodrigues">
-                                    <FaLinkedin size={30} color="#4545E5" />
-                                </a>
-                            </li>
-                            <li>
-
-                                <a target="blank" href="https://github.com/Djaysson">
-                                    <FaGithubSquare size={30} color="#4545E5" />
-                                </a>
-                            </li>
-                            <li>
-
-                                <a target="blank" href="https://www.youtube.com/channel/UCAkanB0GDpy5UfRQnU2zbBg">
-                                    <FaYoutubeSquare size={30} color="#4545E5" />
-                                </a>
-                            </li>
-                        </ul>
-                    </div>
-                </ContainerContato>
-                <ContainerForm action="">
-                    <label>Envie-me uma mensagem</label>
-                    <div>
-                        <input type="text" id="name" name="name" placeholder="Nome" required />
-                        <input type="email" id="email" name="email" placeholder="Email" required />
-                    </div>
-                    <textarea name="message" id="message" placeholder="Conte-me mais sobre sua necessidade....." required />
-                    <Button id="button" type="submit" value="Enviar" />
-
-                </ContainerForm>
-            </Section>
-            <Footer />
-        </Container>
-        </>
-    );
+  return (
+    <>
+      <AnimatedBackground />
+      <Container>
+        <HeaderLogo />
+        <TitlePage subtitle="Contato" title="Entrar em contato" />
+        <Section>
+          <ContainerContato>
+            <div>
+              <h1>Contato alternativo</h1>
+              <ul>
+                <li>
+                  <a target="blank" href="tel:(85)99177-4335">
+                    <FaPhoneSquareAlt size={35} color="#4545E5" />
+                  </a>
+                  <a target="blank" href="tel:(85)99177-4335">
+                    (85)99177-4335
+                  </a>
+                </li>
+                <li>
+                  <a target="blank" href="tel:(85)99177-4335">
+                    <FaWhatsappSquare size={35} color="#4545E5" />
+                  </a>
+                  <a
+                    target="blank"
+                    href={"https://api.whatsapp.com/send?phone=5585991774335"}
+                  >
+                    Whatsapp
+                  </a>
+                </li>
+                <li>
+                  <a target="blank" href="tel:(85)99177-4335">
+                    <FaEnvelopeSquare size={35} color="#4545E5" />
+                  </a>
+                  <a target="blank" href="mailto:djayson.dev@gmail.com">
+                    Email
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <div>
+              <h1>Me siga</h1>
+              <ul>
+                <li>
+                  <a target="blank" href="https://instagram.com/djayson_alves">
+                    <FaInstagramSquare size={35} color="#4545E5" />
+                  </a>
+                </li>
+                <li>
+                  <a
+                    target="blank"
+                    href="https://www.linkedin.com/in/djaysonrodrigues"
+                  >
+                    <FaLinkedin size={35} color="#4545E5" />
+                  </a>
+                </li>
+                <li>
+                  <a target="blank" href="https://github.com/Djaysson">
+                    <FaGithubSquare size={35} color="#4545E5" />
+                  </a>
+                </li>
+                <li>
+                  <a
+                    target="blank"
+                    href="https://www.youtube.com/channel/UCAkanB0GDpy5UfRQnU2zbBg"
+                  >
+                    <FaYoutubeSquare size={35} color="#4545E5" />
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </ContainerContato>
+          <ContainerForm action="">
+            <label>Envie uma mensagem</label>
+            <div>
+              <input
+                type="text"
+                id="name"
+                name="name"
+                placeholder="Nome"
+                required
+              />
+              <input
+                type="email"
+                id="email"
+                name="email"
+                placeholder="Email"
+                required
+              />
+            </div>
+            <textarea
+              name="message"
+              id="message"
+              placeholder="Conte-me mais sobre sua necessidade....."
+              required
+            />
+            <Button id="button" type="submit" value="Enviar" />
+          </ContainerForm>
+        </Section>
+        {/* <Footer /> */}
+      </Container>
+    </>
+  );
 }
 
 export default Contato;
--- src/pages/contato/styles.js
+++ src/pages/contato/styles.js
@@ -1,174 +1,413 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const Container = styled.div`
   width: 100vw;
   height: 100vh;
-  animation: bounceInDown; 
-  animation-duration: 2s; 
+  animation: bounceInDown;
+  animation-duration: 2s;
+  overflow: hidden;
+
+  @media (min-width: 768px) and (max-width: 1024px) {
+    overflow-y: initial;
+  }
+  @media (min-width: 300px) and (max-width: 767px) {
+    overflow-y: initial;
+  }
 `;
 export const Section = styled.section`
   display: flex;
   flex-direction: row;
   justify-content: space-between;
   margin-top: 180px;
-  padding: 20px;
-  
+  padding: 0px 30px;
+  @media (min-width: 768px) and (max-width: 1024px) {
+    overflow-y: initial;
+    margin-top: 150px;
+    padding: 0px 20px;
+  }
+  @media (min-width: 300px) and (max-width: 767px) {
+    display: flex;
+    flex-direction: column-reverse;
+    align-items: center;
+    margin-top: 80px;
+    padding: 0px 5px;
+  }
 `;
+
 export const ContainerContato = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  z-index: 1;
 
-display: flex;
-flex-direction: column;
-z-index: 1;
+  div:first-child {
+    width: 18vw;
 
-div:first-child{
-  margin-left: 20px;
-    
-  h1{
-    font-size: 18px;
-    font-weight: 500;
-    padding-bottom: 10px;
-    text-transform: uppercase;
+    margin-bottom: 50px;
+
+    h1 {
+      font-size: 1.6em;
+      font-weight: 500;
+      padding: 10px 0px;
+      text-transform: capitalize;
     }
-  ul{
-    list-style: none;  
-    display: flex;
-    flex-direction: column;  
+    ul {
+      list-style: none;
+      display: flex;
+      flex-direction: column;
+      align-items: flex-start;
+    }
+    li {
+      display: flex;
+      align-items: center;
+      transition: opacity 0.2s;
+
+      &:hover {
+        opacity: 0.4;
+      }
+      a {
+        font-size: 1em;
+        font-weight: 400;
+        color: ${(props) => props.theme.fontLight};
+      }
+      a:nth-child(2) {
+        padding-left: 5px;
+      }
+    }
   }
-  li{
+
+  div:last-child {
+    width: 18vw;
+    h1 {
+      text-align: center;
+      font-size: 1.6em;
+      font-weight: 500;
+      text-transform: capitalize;
+      padding: 10px 0px;
+    }
+    ul {
+      list-style: none;
+      display: flex;
+      flex-direction: row;
+      justify-content: space-around;
+    }
+    li {
+      transition: opacity 0.2s;
+      &:hover {
+        opacity: 0.4;
+      }
+    }
+  }
+  @media (min-width: 768px) and (max-width: 1024px) {
     display: flex;
+    flex-direction: column;
     align-items: center;
-    transition: opacity 0.2s;
-    svg{
-      box-shadow: 0px 0px 3px ${props => props.theme.darkTransparent};
-    }
-    &:hover{
-    opacity: 0.4;
-    }
-    a{
-    margin-left: 10px;
-    font-size: 17px;
-    font-weight: 500;
-    color: ${props => props.theme.fontLight};
-    }
-  }
-}
+    z-index: 0;
+    div:first-child {
+      width: 20vw;
+      margin: 0px;
+      margin-bottom: 50px;
 
-div:last-child{
-  margin-left: 20px;
-  
-  h1{
-    text-align: center;
-    font-size: 18px;
-    font-weight: 500;
-    text-transform: uppercase;
-    margin-top: 50px;
-    padding-bottom: 10px;
+      h1 {
+        font-size: 1.5em;
+        font-weight: 500;
+        padding: 10px 0px;
+        text-align: center;
+        text-transform: capitalize;
+      }
+      ul {
+        list-style: none;
+        display: flex;
+        flex-direction: row;
+        justify-content: space-around;
+        align-items: center;
+      }
+      li {
+        display: flex;
+        align-items: center;
+        transition: opacity 0.2s;
+        &:hover {
+          opacity: 0.4;
+        }
+        a {
+          font-weight: 400;
+          color: ${(props) => props.theme.fontLight};
+        }
+        a:nth-child(2) {
+          display: none;
+        }
+      }
     }
-  ul{
-    list-style: none;  
-    display: flex;
-    flex-direction: row;    
+    div:last-child {
+      width: 20vw;
+      h1 {
+        text-align: center;
+        font-size: 1.5em;
+        font-weight: 500;
+        text-transform: capitalize;
+        padding: 10px 0px;
+      }
+      ul {
+        list-style: none;
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: space-around;
+      }
+      li {
+        transition: opacity 0.2s;
+        &:hover {
+          opacity: 0.4;
+        }
+      }
+    }
   }
-  li{
+  @media (min-width: 300px) and (max-width: 767px) {
     display: flex;
+    flex-direction: column;
     align-items: center;
-    margin: auto;
-    transition: opacity 0.2s;
-    svg{
-      box-shadow: 0px 0px 3px ${props => props.theme.darkTransparent};
+    animation: fadeInLeft;
+    animation-duration: 2s;
+    z-index: 0;
+    div:first-child {
+      width: 50vw;
+      margin: 50px 0;
+
+      h1 {
+        font-size: 1.4em;
+        font-weight: 500;
+        padding: 10px 0px;
+        text-align: center;
+        text-transform: capitalize;
+      }
+      ul {
+        list-style: none;
+        display: flex;
+        flex-direction: row;
+        justify-content: space-evenly;
+        align-items: center;
+      }
+      li {
+        display: flex;
+        align-items: center;
+        transition: opacity 0.2s;
+        &:hover {
+          opacity: 0.4;
+        }
+        a {
+          font-weight: 400;
+          color: ${(props) => props.theme.fontLight};
+        }
+        a:nth-child(2) {
+          display: none;
+        }
+      }
     }
-    &:hover{
-    opacity: 0.4;
-    }
-    a{
-    margin-left: 10px;
-    
+
+    div:last-child {
+      width: 70vw;
+      margin-bottom: 50px;
+
+      h1 {
+        text-align: center;
+        font-size: 1.4em;
+        font-weight: 500;
+        text-transform: capitalize;
+        padding: 10px 0px;
+      }
+      ul {
+        list-style: none;
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: space-evenly;
+      }
+      li {
+        transition: opacity 0.2s;
+        &:hover {
+          opacity: 0.4;
+        }
+      }
     }
   }
-
-}
-  
 `;
 export const ContainerForm = styled.form`
   display: flex;
   flex-direction: column;
-  margin-right: 30px;
-  width: 55vw;
+  width: 57vw;
   z-index: 1;
-  label{
-    margin-bottom: 5px;
-    font-size: 18px;
-    font-weight: 500; 
-    text-transform: uppercase;
+  label {
+    padding: 5px 20px;
+    font-size: 1.6em;
+    font-weight: 500;
+    text-transform: capitalize;
   }
-  div{
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-    input{
+  div {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-around;
+    input {
       border: none;
-      border-bottom: 4px solid ${props => props.theme.secondary};
-      background:${props => props.theme.itemBackgroundColor};
-      width: 27vw;
-      height: 7vh;
+      border-bottom: 4px solid ${(props) => props.theme.secondary};
+      background: ${(props) => props.theme.itemBackgroundColor};
+      width: 26vw;
+      height: 60px;
       padding-left: 10px;
-      border-radius: 5px;
-      margin-bottom: 12px;
-      transition:  border 0.2s;
-      &::placeholder{
-        color: ${props => props.theme.fontLight};
+      border-radius: 10px;
+      margin-bottom: 20px;
+      transition: border 0.2s;
+      &::placeholder {
+        color: ${(props) => props.theme.fontLight};
         font-weight: 400;
         opacity: 0.3;
       }
-      &:focus{
+      &:focus {
         border: none;
-        background:${props => props.theme.itemBackgroundColor};
-        border-bottom: 4px solid ${props => props.theme.primary};
+        background: ${(props) => props.theme.itemBackgroundColor};
+        border-bottom: 4px solid ${(props) => props.theme.primary};
       }
       &:-webkit-autofill {
-        box-shadow: 0 0 0 30px ${props => props.theme.itemBackgroundColor} inset;
-      -webkit-text-fill-color: ${props => props.theme.fontLight} !important;
-    }
+        box-shadow: 0 0 0 30px ${(props) => props.theme.itemBackgroundColor}
+          inset;
+        -webkit-text-fill-color: ${(props) => props.theme.fontLight} !important;
+      }
     }
   }
 
-  textarea{
+  textarea {
+    margin: 0 auto;
     resize: none;
     width: 55vw;
-    height: 35vh;
+    height: 250px;
     border: none;
-    border-bottom: 4px solid ${props => props.theme.secondary};
-    background:${props => props.theme.itemBackgroundColor};
+    border-bottom: 4px solid ${(props) => props.theme.secondary};
+    background: ${(props) => props.theme.itemBackgroundColor};
     padding: 10px;
-    margin-bottom: 12px;
     border-radius: 10px;
-    transition:  border 0.2s;
-    &::placeholder{
-      color: ${props => props.theme.fontLight};
-        font-weight: 400;
-        opacity: 0.3;
-      }
-      &:focus{
+    transition: border 0.2s;
+    &::placeholder {
+      color: ${(props) => props.theme.fontLight};
+      font-weight: 400;
+      opacity: 0.3;
+    }
+    &:focus {
+      border: none;
+      background: ${(props) => props.theme.itemBackgroundColor};
+      border-bottom: 4px solid ${(props) => props.theme.primary};
+    }
+  }
+  @media (min-width: 768px) and (max-width: 1024px) {
+    overflow-y: initial;
+    display: flex;
+    flex-direction: column;
+    width: 70vw;
+    z-index: 0;
+    label {
+      padding: 10px 0px;
+      text-align: left;
+      font-size: 1.5em;
+      font-weight: 500;
+      text-transform: capitalize;
+    }
+    div {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      input {
         border: none;
-        background:${props => props.theme.itemBackgroundColor};
-        border-bottom: 4px solid ${props => props.theme.primary};
+        border-bottom: 4px solid ${(props) => props.theme.secondary};
+        background: ${(props) => props.theme.itemBackgroundColor};
+        width: 70vw;
+        height: 50px;
+        padding-left: 10px;
+        border-radius: 10px;
+        margin-bottom: 20px;
       }
     }
+
+    textarea {
+      margin: 0 auto;
+      resize: none;
+      width: 70vw;
+      height: 200px;
+      border: none;
+      border-bottom: 4px solid ${(props) => props.theme.secondary};
+      background: ${(props) => props.theme.itemBackgroundColor};
+      padding: 10px;
+      border-radius: 10px;
+      transition: border 0.2s;
+    }
+  }
+  @media (min-width: 300px) and (max-width: 767px) {
+    display: flex;
+    flex-direction: column;
+    width: 100vw;
+    z-index: 0;
+    animation: fadeInLeft;
+    animation-duration: 2s;
+    label {
+      padding: 10px 0px;
+      text-align: center;
+      font-size: 1.4em;
+      font-weight: 500;
+      text-transform: capitalize;
+    }
+    div {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      input {
+        border: none;
+        border-bottom: 4px solid ${(props) => props.theme.secondary};
+        background: ${(props) => props.theme.itemBackgroundColor};
+        width: 80vw;
+        height: 40px;
+        padding-left: 10px;
+        border-radius: 10px;
+        margin-bottom: 18px;
+        transition: border 0.2s;
+      }
+    }
+
+    textarea {
+      margin: 0 auto;
+      resize: none;
+      width: 80vw;
+      height: 150px;
+      border: none;
+      border-bottom: 4px solid ${(props) => props.theme.secondary};
+      background: ${(props) => props.theme.itemBackgroundColor};
+      padding: 10px;
+      border-radius: 10px;
+      transition: border 0.2s;
+    }
+  }
 `;
 export const Button = styled.input`
-  width: 10vw;
-  height: 6vh;
-  margin: auto;
+  width: 25vw;
+  height: 40px;
+  margin: 20px auto;
   border: none;
   border-radius: 10px;
   font-weight: 500;
-  background:${props => props.theme.secondary};
-  box-shadow: 0px 0px 15px ${props => props.theme.darkTransparent};
-  transition:  transform 1s;
-    &:hover{
-      background: ${props => props.theme.primary};
-      -ms-transform: scale(1.1); 
-      -webkit-transform: scale(1.1); 
-      transform: scale(1.1);
-    }
+  background: ${(props) => props.theme.secondary};
+  box-shadow: 0px 0px 15px ${(props) => props.theme.darkTransparent};
+  transition: transform 1s;
+  &:hover {
+    background: ${(props) => props.theme.primary};
+    -ms-transform: scale(1.1);
+    -webkit-transform: scale(1.1);
+    transform: scale(1.1);
+  }
+  @media (min-width: 768px) and (max-width: 1024px) {
+    width: 24.5vw;
+    height: 40px;
+    margin: 20px auto;
+  }
+  @media (min-width: 300px) and (max-width: 767px) {
+    width: 24vw;
+    height: 40px;
+    margin: 18px auto;
+  }
 `;
