29b5f932ead9ad1295a97d76db4a96c729793a81
Djaysson <djayson.dev@gmail.com>
2021-09-27 21:51:06 -0300

    Add responsive design to loading component

--- src/components/loading/styles.js
+++ src/components/loading/styles.js
@@ -1,27 +1,57 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const Container = styled.div`
- display: flex;
- align-items: center;
- justify-content: center;
- height: calc(100vh - 10px);
- h1{  
-   text-align: center;
-   font-size: 15px;
-   animation: fadeIn 1s alternate infinite;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: calc(90vh - 10px);
+  overflow: hidden;
+  h1 {
+    text-align: center;
+    font-size: 0.8em;
+    animation: fadeIn 1s alternate infinite;
+  }
+  @media (min-width: 768px) and (max-width: 1024px) {
+    height: calc(70vh - 10px);
+    h1 {
+      font-size: 0.7em;
+    }
+  }
+  @media (min-width: 300px) and (max-width: 767px) {
+    height: calc(70vh - 10px);
+    h1 {
+      font-size: 0.6em;
+    }
   }
 `;
 export const Spinner = styled.div`
- border: 8px solid ${props => props.theme.TransparentBackground};
- box-shadow: 0px 0px 5px ${props => props.theme.darkTransparent};
- border-left-color:  ${props => props.theme.secondary};
- height: 130px;
- width: 130px;
- border-radius: 50%;
- position: absolute;
- animation: spin 1s linear infinite;
- @keyframes spin{
-     to{transform: rotate(360deg)}
- }
-  
+  border: 6px solid ${(props) => props.theme.TransparentBackground};
+  box-shadow: 0px 0px 5px ${(props) => props.theme.darkTransparent};
+  border-left-color: ${(props) => props.theme.secondary};
+  height: 110px;
+  width: 110px;
+  border-radius: 50%;
+  position: absolute;
+  animation: spin 1s linear infinite;
+  @keyframes spin {
+    to {
+      transform: rotate(360deg);
+    }
+  }
+  @media (min-width: 768px) and (max-width: 1024px) {
+    border: 5px solid ${(props) => props.theme.TransparentBackground};
+    border-left-color: ${(props) => props.theme.secondary};
+    height: 90px;
+    width: 90px;
+    position: absolute;
+    animation: spin 1s linear infinite;
+  }
+  @media (min-width: 300px) and (max-width: 767px) {
+    border: 5px solid ${(props) => props.theme.TransparentBackground};
+    border-left-color: ${(props) => props.theme.secondary};
+    height: 70px;
+    width: 70px;
+    position: absolute;
+    animation: spin 1s linear infinite;
+  }
 `;
