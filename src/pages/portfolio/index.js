d1a68615d57f084d27580c1912a29e485494bbd3
Djaysson <djayson.dev@gmail.com>
2021-09-27 21:49:28 -0300

    Adding responsive design to portfolio page

--- src/pages/portfolio/index.js
+++ src/pages/portfolio/index.js
@@ -1,63 +1,74 @@
-import React, { useState, useEffect } from 'react';
-import HeaderLogo from '../../components/headerLogo/index'
+import React, { useState, useEffect } from "react";
+import HeaderLogo from "../../components/headerLogo/index";
 import api from "../../server/api";
-import { Container, ContainerImg, ContainerButtons } from './styles';
-import { FaLaptopCode, FaGithub, } from 'react-icons/fa'
-import TitlePage from '../../components/titlePage/index';
-import AnimatedBackground from '../../components/background/index'
-import Loading from '../../components/loading/index'
+import { Container, ContainerImg, ContainerButtons } from "./styles";
+import { FaLaptopCode, FaGithub } from "react-icons/fa";
+import TitlePage from "../../components/titlePage/index";
+import AnimatedBackground from "../../components/background/index";
+import Loading from "../../components/loading/index";
 
 function Portfolio() {
-    const [data, setData] = useState([]);
-    const [load,setLoad] = useState(true);
-    useEffect(() => {
-        const timer = setTimeout(() => {
-        const fetchData = async () => {
-            const response = await api.get('/projetos');
-            const data = response.data;
-            setData(data);
-        }
-        fetchData();
-        setLoad(false);
-    }, 3000);
+  const [data, setData] = useState([]);
+  const [load, setLoad] = useState(true);
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      const fetchData = async () => {
+        const response = await api.get("/projetos");
+        const data = response.data;
+        setData(data);
+      };
+      fetchData();
+      setLoad(false);
+    }, 2000);
 
     return () => clearTimeout(timer);
-    }, []);
+  }, []);
 
-    return (
-        <>
-        <AnimatedBackground />
-        <Container>
-            <HeaderLogo />
-            <TitlePage subtitle="Portfolio" title="Meus projetos" />
+  return (
+    <>
+      <AnimatedBackground />
+      <Container>
+        <HeaderLogo />
+        <TitlePage subtitle="Portfolio" title="Meus projetos" />
 
-                {load && <Loading/>}
-                {!load  &&
-                <ContainerImg >
-                    {data.map(projetos => (
-                        <article key={projetos.id} >
-                            <div>
-                                <div >
-                                    <strong>{projetos.title}</strong>
-                                    <p>Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos.indústria tipográfica e de impressos.indústria tipográfica e de impressos.</p>
-                                    <p>HTML - CSS - JS - REACT</p>
-                                    <span>
-                                        <ContainerButtons>
-                                            <strong><a href={projetos.linkGithub} target="blank" ><FaGithub size={12} /> Github</a></strong>
-                                            <strong><a href={projetos.linkDemo} target="blank"><FaLaptopCode size={12} /> Demo</a></strong>
-                                        </ContainerButtons>
-                                    </span>
-                                </div>
-                                <img src={projetos.image} alt="imagem do projeto" />
-                            </div>
-                        </article>
-                    ))}
-                </ContainerImg>
-                }
-        </Container>
-                </>
-
-    );
+        {load && <Loading />}
+        {!load && (
+          <ContainerImg>
+            {data.map((projetos) => (
+              <article key={projetos.id}>
+                <div>
+                  <div>
+                    <strong>{projetos.title}</strong>
+                    <p>
+                      Lorem Ipsum é simplesmente uma simulação de texto da
+                      indústria tipográfica e de impressos.indústria tipográfica
+                      e de impressos.indústria tipográfica e de impressos.
+                    </p>
+                    <p>HTML - CSS - JS - REACT</p>
+                    <span>
+                      <ContainerButtons>
+                        <strong>
+                          <a href={projetos.linkGithub} target="blank">
+                            <FaGithub size={12} /> Github
+                          </a>
+                        </strong>
+                        <strong>
+                          <a href={projetos.linkDemo} target="blank">
+                            <FaLaptopCode size={12} /> Demo
+                          </a>
+                        </strong>
+                      </ContainerButtons>
+                    </span>
+                  </div>
+                  <img src={projetos.image} alt="imagem do projeto" />
+                </div>
+              </article>
+            ))}
+          </ContainerImg>
+        )}
+      </Container>
+    </>
+  );
 }
 
 export default Portfolio;
--- src/pages/portfolio/styles.js
+++ src/pages/portfolio/styles.js
@@ -1,95 +1,241 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const Container = styled.div`
   width: 100vw;
   height: 100vh;
-  animation: bounceInDown; 
-  animation-duration: 2s; 
+  animation: bounceInDown;
+  animation-duration: 2s;
+  overflow-x: hidden;
+  overflow-y: initial;
 `;
 export const ContainerImg = styled.section`
   @keyframes animed {
-      0% {
-        transform:  rotatey(5deg);
-        filter: drop-shadow(2px 2px 6px ${props => props.theme.primary});
-      }
-      100% {
-        transform: rotateY(-30deg);
-      }
+    0% {
+      filter: drop-shadow(0px 0px 5px ${(props) => props.theme.secondary});
     }
-
-  width: 60vw;
+    100% {
+      filter: drop-shadow(0px 0px 5px ${(props) => props.theme.primary});
+    }
+  }
   display: grid;
-  margin-left: 10em;
-  justify-content: center;
-  grid-template-columns: repeat(4,1fr);
-  grid-gap: 1em;
-  margin-top: 110px;
+  grid-template-columns: repeat(3, 1fr);
+  grid-gap: 10px;
+  margin-top: 120px;
+  padding: 0 30px;
   article {
-    width: 16vw; 
-    height: 38vh;
+    margin: 10px auto;
+    width: 24vw;
+    height: 270px;
     z-index: 1;
     animation: animed 4s alternate infinite;
-    div{
+    div {
       display: flex;
       flex-direction: column;
       align-items: center;
-      justify-content: center; 
-      img{
-        width: 16vw; 
-        height: 38vh;
-        border-radius: 10px;    
-      }
-      div{ 
-        position: absolute;    
-        width: 16vw;
-        height: 38vh;
+      justify-content: center;
+      img {
+        width: 24vw;
+        height: 270px;
         border-radius: 10px;
-        strong{     
-          font-size: 25px;
+      }
+      div {
+        position: absolute;
+        width: 24vw;
+        height: 270px;
+        border-radius: 10px;
+        strong {
+          font-size: 1.4em;
           font-weight: 700;
-          color: ${props => props.theme.secondary}; 
+          color: ${(props) => props.theme.secondary};
           visibility: hidden;
         }
-        span{        
+        span {
           visibility: hidden;
         }
-        p:nth-child(2){
-          font-size: 13px;
+        p:nth-child(2) {
+          font-size: 0.8em;
           font-weight: 700;
           margin: 10px;
           text-align: center;
-          color: ${props => props.theme.font}; 
+          color: ${(props) => props.theme.font};
           visibility: hidden;
         }
-        p:nth-child(3){
-          visibility: hidden;  
-          font-size: 9px;
+        p:nth-child(3) {
+          visibility: hidden;
+          font-size: 0.7em;
           padding: 4px;
-          border-bottom: 5px solid ${props => props.theme.secondary};
-          box-shadow: 0px 0px 15px ${props => props.theme.darkTransparent};
+          border-bottom: 5px solid ${(props) => props.theme.secondary};
+          box-shadow: 0px 0px 15px ${(props) => props.theme.darkTransparent};
           border-radius: 5px;
-          background: ${props => props.theme.itemBackgroundColor};   
+          background: ${(props) => props.theme.itemBackgroundColor};
           margin: 10px 0px;
         }
-        &:hover{     
-          background: ${props => props.theme.TransparentBackground};
-          border-bottom: 10px solid ${props => props.theme.secondary};
-          strong{
-              visibility:visible;         
+        &:hover {
+          background: ${(props) => props.theme.TransparentBackground};
+          border-bottom: 10px solid ${(props) => props.theme.secondary};
+          strong {
+            visibility: visible;
           }
-          span{
-              visibility:visible;
+          span {
+            visibility: visible;
           }
-          p:nth-child(2){
-            visibility:visible;
+          p:nth-child(2) {
+            visibility: visible;
           }
-          p:nth-child(3){
-            visibility:visible;
-
+          p:nth-child(3) {
+            visibility: visible;
           }
-        } 
+        }
       }
-    }  
+    }
+  }
+  @media (min-width: 768px) and (max-width: 1024px) {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    grid-gap: 10px;
+    margin-top: 120px;
+    padding: 0 30px;
+    article {
+      margin: 10px auto;
+      width: 40vw;
+      height: 270px;
+      z-index: 0;
+      animation: animed 4s alternate infinite;
+      div {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        img {
+          width: 40vw;
+          height: 270px;
+          border-radius: 10px;
+        }
+        div {
+          position: absolute;
+          width: 40vw;
+          height: 270px;
+          border-radius: 10px;
+          strong {
+            font-size: 1.4em;
+            font-weight: 700;
+            color: ${(props) => props.theme.secondary};
+            visibility: hidden;
+          }
+          span {
+            visibility: hidden;
+          }
+          p:nth-child(2) {
+            font-size: 0.8em;
+            font-weight: 700;
+            margin: 10px;
+            text-align: center;
+            color: ${(props) => props.theme.font};
+            visibility: hidden;
+          }
+          p:nth-child(3) {
+            visibility: hidden;
+            font-size: 0.7em;
+            padding: 4px;
+            border-bottom: 5px solid ${(props) => props.theme.secondary};
+            box-shadow: 0px 0px 15px ${(props) => props.theme.darkTransparent};
+            border-radius: 5px;
+            background: ${(props) => props.theme.itemBackgroundColor};
+            margin: 10px 0px;
+          }
+          &:hover {
+            background: ${(props) => props.theme.TransparentBackground};
+            border-bottom: 10px solid ${(props) => props.theme.secondary};
+            strong {
+              visibility: visible;
+            }
+            span {
+              visibility: visible;
+            }
+            p:nth-child(2) {
+              visibility: visible;
+            }
+            p:nth-child(3) {
+              visibility: visible;
+            }
+          }
+        }
+      }
+    }
+  }
+  @media (min-width: 300px) and (max-width: 767px) {
+    display: grid;
+    grid-template-columns: repeat(1, 1fr);
+    grid-gap: 10px;
+    margin-top: 80px;
+    padding: 0px;
+    article {
+      width: 80vw;
+      height: 250px;
+      z-index: 0;
+      margin-bottom: 20px;
+      animation: animed 4s alternate infinite;
+      div {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        img {
+          width: 80vw;
+          height: 250px;
+          border-radius: 10px;
+        }
+        div {
+          position: absolute;
+          width: 80vw;
+          height: 250px;
+          border-radius: 10px;
+          strong {
+            font-size: 1.5em;
+            font-weight: 700;
+            color: ${(props) => props.theme.secondary};
+            visibility: hidden;
+          }
+          span {
+            visibility: hidden;
+          }
+          p:nth-child(2) {
+            font-size: 1.1em;
+            font-weight: 700;
+            margin: 10px;
+            text-align: center;
+            color: ${(props) => props.theme.font};
+            visibility: hidden;
+          }
+          p:nth-child(3) {
+            visibility: hidden;
+            font-size: 1em;
+            padding: 4px;
+            border-bottom: 5px solid ${(props) => props.theme.secondary};
+            box-shadow: 0px 0px 15px ${(props) => props.theme.darkTransparent};
+            border-radius: 5px;
+            background: ${(props) => props.theme.itemBackgroundColor};
+            margin: 10px 0px;
+          }
+          &:hover {
+            background: ${(props) => props.theme.TransparentBackground};
+            border-bottom: 10px solid ${(props) => props.theme.secondary};
+            strong {
+              visibility: visible;
+            }
+            span {
+              visibility: visible;
+            }
+            p:nth-child(2) {
+              visibility: visible;
+            }
+            p:nth-child(3) {
+              visibility: visible;
+            }
+          }
+        }
+      }
+    }
   }
 `;
 export const ContainerButtons = styled.footer`
@@ -98,38 +244,72 @@ export const ContainerButtons = styled.footer`
   align-items: center;
   justify-content: center;
   margin-top: 5px;
-  a{
-    color: ${props => props.theme.font};
+  a {
+    color: ${(props) => props.theme.font};
   }
-  strong:first-child{
-    background: ${props => props.theme.secondary};
-    font-size: 14px;
+  strong:first-child {
+    background: ${(props) => props.theme.secondary};
+    font-size: 0.9em;
     font-weight: 700;
     padding: 5px;
     margin-right: 7px;
     align-items: center;
     border-radius: 10px;
-    transition: transform .2s;
-    &:hover{
-      background: ${props => props.theme.primary};
-        -ms-transform: scale(1.1); 
-        -webkit-transform: scale(1.1); 
-        transform: scale(1.1);
-    } 
+    transition: transform 0.2s;
+    &:hover {
+      background: ${(props) => props.theme.primary};
+      -ms-transform: scale(1.1);
+      -webkit-transform: scale(1.1);
+      transform: scale(1.1);
+    }
   }
-  strong:last-child{
-    background: ${props => props.theme.secondary};
-    font-size: 14px;
+  strong:last-child {
+    background: ${(props) => props.theme.secondary};
+    font-size: 0.9em;
     font-weight: 700;
     padding: 5px;
     align-items: center;
     border-radius: 10px;
-    transition: transform .2s;
-    &:hover{
-      background: ${props => props.theme.primary};
-      -ms-transform: scale(1.1); 
-      -webkit-transform: scale(1.1); 
+    transition: transform 0.2s;
+    &:hover {
+      background: ${(props) => props.theme.primary};
+      -ms-transform: scale(1.1);
+      -webkit-transform: scale(1.1);
       transform: scale(1.1);
+    }
+  }
+
+  @media (min-width: 300px) and (max-width: 767px) {
+    strong:first-child {
+      background: ${(props) => props.theme.secondary};
+      font-size: 1.1em;
+      font-weight: 700;
+      padding: 5px;
+      margin-right: 7px;
+      align-items: center;
+      border-radius: 10px;
+      transition: transform 0.2s;
+      &:hover {
+        background: ${(props) => props.theme.primary};
+        -ms-transform: scale(1.1);
+        -webkit-transform: scale(1.1);
+        transform: scale(1.1);
+      }
+    }
+    strong:last-child {
+      background: ${(props) => props.theme.secondary};
+      font-size: 1.1em;
+      font-weight: 700;
+      padding: 5px;
+      align-items: center;
+      border-radius: 10px;
+      transition: transform 0.2s;
+      &:hover {
+        background: ${(props) => props.theme.primary};
+        -ms-transform: scale(1.1);
+        -webkit-transform: scale(1.1);
+        transform: scale(1.1);
+      }
     }
   }
 `;
